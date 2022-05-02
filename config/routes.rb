cd0584b7d7f30fa647fe1e4b64e4c6e63ed79981
JunhoYoon95 <enffl95@icloud.com>
2022-05-02 18:00:02 +0900

    처음으로 루비의 구조들을 이해해 보고 있습니다. Notion의 Ruby 페이지와 함께 공부를 하고 있으니 그 부분을 같이 보면 좋을 것 같아요

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get '/' => 'home#index' 
+  # 슬래시(/)로 접속을 하게 되면, home 안에 들어있는 index 액션으로 작업을 하도록 만들어라
+  get '/add' => 'calculator#add'
+end
