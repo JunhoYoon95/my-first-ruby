2f78744b52d541bdb34df7b9216827064284586c
JunhoYoon95 <enffl95@icloud.com>
2022-05-09 10:42:14 +0900

    Ruby 에서 사용하게 될 CRUD 를 배워 보았습니다. redirect_to :back 은 이제 지원하지 않으므로 redirect_back fallback_location: 주소 로 사용합니다

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,40 @@
 class HomeController < ApplicationController
     def index
-    
+        @posts = Post.all
+        # Post 한 모든 모델을 불러오는 것 여러개를 한번에 가져오는거임
+        # 인덱스 페이지에서 정보가 보이도록 할 것임.
     end
+    def write
+
+    end
+    def create
+        post = Post.new
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+        redirect_to '/index'
+    end
+
+    def modify
+        @post = Post.find(params[:post_id])
+        # db에서 그 id 를 갖는 녀석을 가져와주겠지
+        # Post.all 과 다른거는 얘는 하나만 가져옵니다
+    end
+
+    def update
+        post = Post.find(params[:post_id])
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save
+        redirect_back(fallback_location: '/index')
+        # redirect_to '/index'
+
+    end
+
+    def delete
+        Post.destroy(params[:post_id])
+
+        redirect_back fallback_location: '/index'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get '/' => 'home#index' 
-  # 슬래시(/)로 접속을 하게 되면, home 안에 들어있는 index 액션으로 작업을 하도록 만들어라
-  get '/add' => 'calculator#add'
-  get '/result' => 'calculator#result'
+  get '/index' => 'home#index' 
+  get '/write' => 'home#write' 
+  post '/create' => 'home#create' 
+  
+  get '/modify/:post_id' => 'home#modify'
+  post '/update/:post_id' => 'home#update'
+  get '/delete/:post_id' => 'home#delete'
 end
